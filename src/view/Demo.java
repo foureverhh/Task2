ab461d46c149317cc81375eb1f902a43edef4f95
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-12-14 00:00:49 +0100

    GUI added

--- src/view/Demo.java
+++ src/view/Demo.java
@@ -4,11 +4,35 @@ import controller.Attendance;
 import model.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Demo  {
-    private static JFrame demo ;
+    //gui components
+    private static JFrame demo;
+    private static JPanel studentInfo;
+    private static JPanel seminarInfo;
+    private static JPanel attendanceInfo;
+    private static JPanel buttonPanel;
+    private static JPanel container;
+
+    private static JRadioButton student1;
+    private static JRadioButton student2;
+    private static JRadioButton student3;
+    private static JRadioButton student4;
+    private static ButtonGroup student_group;
+
+    private static JRadioButton seminar1;
+    private static JRadioButton seminar2;
+    private static ButtonGroup seminar_group;
+
+    private static JButton check_result;
+
+    private static JTextArea result;
+
+    //class fields
     private static List<Student>  students;
     private static List<Seminar> seminars;
     private static List<Teacher> teachers;
@@ -36,16 +60,76 @@ public class Demo  {
     private static Teacher sigrun;
 
     public static void main(String[] args) {
-        demo = new JFrame("Seminar Demo");
-
+        //Init all java class instances
         initData();
+        guiConfig();
 
-        Attendance attendance1 = new Attendance();
+ /*       Attendance attendance1 = new Attendance();
         attendance1.registerAttendance(objectOrientedProgramming, stu4);
         System.out.println("Seminar attendance: " + objectOrientedProgramming);
         System.out.println("Seminar total Attendance " + objectOrientedProgramming.getTotalAttendance());
-        System.out.println("Student SeminarAttended(): " + stu4.getSeminarsAttended());
+        System.out.println("Student SeminarAttended(): " + stu4.getSeminarsAttended());*/
 
+    }
+
+    static void guiConfig(){
+        demo = new JFrame("Seminar Demo");
+        demo.setSize(800,800);
+        demo.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        demo.setLocationRelativeTo(null);
+
+        //panels
+        container = new JPanel();
+        studentInfo = new JPanel();
+        studentInfo.setBorder(BorderFactory.createTitledBorder("Students"));
+        seminarInfo = new JPanel();
+        seminarInfo.setBorder(BorderFactory.createTitledBorder("Seminars"));
+        buttonPanel = new JPanel();
+        attendanceInfo = new JPanel();
+        attendanceInfo.setBorder(BorderFactory.createTitledBorder("Attendance"));
+
+        //students radio buttons
+        student1 = new JRadioButton("Stu1");
+        student1.setSelected(true);
+        student2 = new JRadioButton("Stu2");
+        student3 = new JRadioButton("Stu3");
+        student4 = new JRadioButton("Stu4");
+        ButtonGroup student_group = new ButtonGroup();
+        student_group.add(student1);
+        student_group.add(student2);
+        student_group.add(student3);
+        student_group.add(student4);
+        studentInfo.add(student1);
+        studentInfo.add(student2);
+        studentInfo.add(student3);
+        studentInfo.add(student4);
+
+        //seminars radio buttons
+        seminar1 = new JRadioButton("Object Oriented Programming");
+        seminar1.setSelected(true);
+        seminar2 = new JRadioButton("Object Analyze and Design");
+        seminar_group = new ButtonGroup();
+        student_group.add(student1);
+        student_group.add(student2);
+        seminarInfo.add(seminar1);
+        seminarInfo.add(seminar2);
+
+        //button
+        check_result = new JButton("Check Attendance Result");
+        buttonPanel.add(check_result);
+
+        //attendance info
+        result = new JTextArea(60,60);
+        //attendanceInfo.setLayout(new BoxLayout(attendanceInfo,BoxLayout.Y_AXIS));
+        attendanceInfo.add(result);
+
+        container.add(studentInfo);
+        container.add(seminarInfo);
+        container.add(buttonPanel);
+        container.add(attendanceInfo);
+        container.setLayout(new BoxLayout(container,BoxLayout.Y_AXIS));
+        demo.add(container);
+        demo.setVisible(true);
     }
 
     static void initData(){
@@ -118,6 +202,4 @@ public class Demo  {
         teachers.add(robert);
         teachers.add(sigrun);
     }
-
-
 }
